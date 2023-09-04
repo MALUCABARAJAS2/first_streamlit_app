53df30c829c81916d3fd39a0d92e482a9afde580
MALUCABARAJAS2 <144021490+MALUCABARAJAS2@users.noreply.github.com>
2023-09-04 13:47:17 +0200

    Correct text code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.subtitle('Breakfast Menu')
 streamlit.text(
-  f'Omega 3 & Blueberry Oatmeal
+  f"""Omega 3 & Blueberry Oatmeal
   Kale, spinach & Rocket Smoothie
-  Hard-Boiled Free-Range Egg')
+  Hard-Boiled Free-Range Egg""")
