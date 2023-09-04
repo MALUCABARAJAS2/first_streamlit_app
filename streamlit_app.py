a55f412ec9d65aa39dbbff1d0c622e01bd78bb03
MALUCABARAJAS2 <144021490+MALUCABARAJAS2@users.noreply.github.com>
2023-09-04 13:46:37 +0200

    Increase description of Breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.subtitle('Breakfast Menu')
+streamlit.text(
+  f'Omega 3 & Blueberry Oatmeal
+  Kale, spinach & Rocket Smoothie
+  Hard-Boiled Free-Range Egg')
